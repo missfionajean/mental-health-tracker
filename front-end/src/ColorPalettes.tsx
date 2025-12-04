5638bc46abba050d7a889ff416f72c7c31662785
missfionajean <missfionajean@gmail.com>
2025-12-04 14:01:53 -0500

    added base palette tsx file to import wherever

---
+++ front-end/src/ColorPalettes.tsx
@@ -0,0 +1,10 @@
+export const basePalette = {
+    excited: '#8EC73F',
+    happy: '#01A774',
+    relaxed: '#00AEEF',
+    bored: '#0054A6',
+    depressed: '#93278F',
+    sad: '#ED0973',
+    angry: '#EC1D25',
+    tense: '#FFC310',
+};
